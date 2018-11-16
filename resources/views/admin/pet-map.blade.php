a121206766972ed8e11ec08cdeeeb0fb26c0da75
biglanggwapoventures <biglanggwapoventures@gmail.com>
2018-11-16 18:40:44 +0800

    pet map

--- resources/views/admin/pet-map.blade.php
+++ resources/views/admin/pet-map.blade.php
@@ -16,17 +16,46 @@
     console.log(data)
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 10.3157, lng: 123.8854},
-      zoom: 15
+      zoom: 10
     });
     var length = data.length
     for(var x = 0; x<length; x++) {
-      console.log(data[x])
-      var marker = new google.maps.Marker({
-        position: {lat: data[x].origin_latitude, lng: data[x].origin_longitude},
-        map: map,
-        title: 'Hello World!'
-      });
+      putMarker(map, data[x])
     }
+  }
+
+  function putMarker (map, item) {
+    var marker = new google.maps.Marker({
+      position: {lat: item.origin_latitude, lng: item.origin_longitude},
+      map: map,
+      title: item.name
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: `</div><img class="rounded mx-auto mb-2" style="width:100px" src="${item.photo_filepath}" />
+      <dl class="row">
+  <dt class="col-sm-3">Name</dt>
+  <dd class="col-sm-9">${item.pet_name}</dd>
+
+  <dt class="col-sm-3">Class</dt>
+  <dd class="col-sm-9">
+      ${item.species} 
+  </dd>
+  <dt class="col-sm-3">Breed</dt>
+  <dd class="col-sm-9">
+      ${item.breed}
+  </dd>
+  <dt class="col-sm-3">Address</dt>
+  <dd class="col-sm-9">
+      ${item.origin}
+  </dd>
+</dl>`,
+        position: {lat: item.origin_latitude, lng: item.origin_longitude},
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(map);
+    });
   }
 </script>
 @endpush
